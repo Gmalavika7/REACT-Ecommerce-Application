50aa32315c71653c41a9d8d23c1555af9e1193a2
Gmalavika7 <malavikabakiya@gmail.com>
2026-01-06 21:12:30 +0530

    React-App: Hero Section with Swipe Animation and Products Section with AOS Animation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,6 +3,55 @@ import Logo from "../../assets/logo.png"
 import { IoMdSearch } from "react-icons/io";
 import { FaCartShopping } from "react-icons/fa6";
 import DarkMode from "./../DarkMode";
+import { FaCaretDown } from "react-icons/fa";
+
+
+const Menu = [
+    {
+        id:1,
+        name: "Home",
+        link: "/#"
+    },
+     {
+        id:2,
+        name: "Top Rates",
+        link: "/#services",
+    },
+     {
+        id:3,
+        name: "Kids Wear",
+        link: "/#",
+    },
+     {
+        id:3,
+        name: "Mens Wear",
+        link: "/#",
+    },
+     {
+        id:3,
+        name: "Electronics",
+        link: "/#",
+    },
+];
+
+const DropdownLinks = [
+    {
+        id: 1,
+        name: "Trending Products",
+        link: "/#",
+    },
+     {
+        id: 2,
+        name: "Best Selling",
+        link: "/#",
+    },
+     {
+        id: 3,
+        name: "Top Rated",
+        link: "/#",
+    },
+];
+
 
 
 
@@ -27,7 +76,9 @@ const Navbar = () => {
                         className="w-[200px] sm:w-[200px]
                         group-hover:w-[300px] transition-all duration-300 
                         rounded-full border border-gray-300 px-2 py-1 
-                        focus:outline-none focus:border-1 focus:border-[var(--primary)] bg-white text-black"/>
+                        focus:outline-none focus:border-1 focus:border-[var(--primary)]
+                        dark:border-gray-500
+                        dark:bg-gray-800"/>
                         <IoMdSearch
                         className='text-gray-500 group-hover:text-[var(--primary)] absolute top-1/2 -translate-y-1/2 right-3 transition-all duration-600'
                         />
@@ -57,7 +108,55 @@ const Navbar = () => {
 
         </div>
         {/* lower Navbar */}
-        <div></div>
+        <div className='flex justify-center'>
+            <ul className='sm:flex hidden items-center gap-4'>
+                {Menu.map((data) =>(
+                        <li key={data.id}>
+                            <a href={data.link}
+                            className='inline-block px-4 hover:text-[var(--primary)] duration-200 '
+                            >{data.name}</a>
+                        </li>
+                    ))
+                }
+                {/* Simple Dropdown and Links */}
+                <li  className='group relative cursor-pointer'>
+                    <a href='#'
+                    className='flex items-center gap[-2px] py-2'
+                    >
+                        Trending 
+                        <span>
+                            <FaCaretDown
+                            className="transition-all
+                            duration-200
+                            group-hover:rotate-180"/>
+                        </span>
+                        
+                    </a>
+                    <div className='absolute z-[9999] hidden
+                    group-hover:block w-[150px] rounded-md
+                    bg-white p-2 text-black shadow-md'>
+                        <ul>
+                            {DropdownLinks.map((data) => (
+
+                                <li key={data.id}>
+                                    <a 
+                                    href={data.link}
+                                    className='inline-block w-full 
+                                    rounded-md p-2
+                                    hover:bg-[var(--primary)]/20'>
+                                        {data.name}
+                                    </a>
+                                </li>
+                            ))
+
+
+                            }
+
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
 
     </div>
   )
